79f4e0362c2773578db2263fbaa68622de08bb79
borisp <borispavec@yahoo.com>
2024-06-10 18:08:30 +0200

    Editing Objects

--- pizza/views.py
+++ pizza/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import PizzaForm, MultiplePizzasForm
 
 from django.forms import formset_factory
+from .models import Pizza
 
 def home(request):
     return render(request, 'pizza/home.html')
@@ -11,10 +12,12 @@ def order(request):
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)
         if filled_form.is_valid():
+            created_pizza = filled_form.save()
+            created_pizza_pk = created_pizza.id
             note = f"Thanks For Ordering! Your {filled_form.cleaned_data['size']} {filled_form.cleaned_data['topping1']} and {filled_form.cleaned_data['topping2']} pizza is on its way"
 
             new_form = PizzaForm()
-            return render(request, 'pizza/order.html', {'pizzaform': new_form, 'note': note, 'multiple_form': multiple_form})
+            return render(request, 'pizza/order.html', {'created_pizza_pk': created_pizza_pk, 'pizzaform': new_form, 'note': note, 'multiple_form': multiple_form})
     else:
         form = PizzaForm()
         return render(request, 'pizza/order.html', {'pizzaform': form, 'multiple_form': multiple_form})
@@ -37,3 +40,15 @@ def pizzas(request):
         return render(request, 'pizza/pizzas.html', {'note': note, 'formset': formset})
     else:
         return render(request, 'pizza/pizzas.html', {'formset': formset})
+    
+def edit_order(request, pk):
+    pizza = Pizza.objects.get(pk=pk)
+    form = PizzaForm(instance=Pizza)
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST, instance=pizza)
+        if filled_form.is_valid():
+            filled_form.save()
+            form = filled_form
+            note = 'Order has been updated.'
+            return render(request, 'pizza/edit_order.html', {'note':note,'pizzaform':form,'pizza':pizza})
+    return render(request, 'pizza/edit_order.html', {'pizzaform': form, 'pizza': pizza})
